6d2acd339c30e90dc8be2449665463b47dce2466
Tiago Almeida de Queiroz <tiago.queiroz@ravex.local>
2025-02-03 10:56:18 -0300

    Gerando módulo, componente e rota de categorias

---
+++ src/app/modules/categories/categories.routing.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+import { CategoriesHomeComponent } from "./pages/categories-home/categories-home.component";
+
+export const CATEGORIES_ROUTING: Routes = [
+  {
+    path: '',
+    component: CategoriesHomeComponent
+  }
+]
